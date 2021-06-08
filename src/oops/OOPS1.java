0984a9ed365bc1787fff58e1055e6ca568e0610f
tagrohan <tagrohan@gmail.com>
2021-06-09 05:17:14 +0530

    ninja started

---
+++ src/oops/OOPS1.java
@@ -0,0 +1,18 @@
+package oops;
+
+public class OOPS1 {
+   public static void main(String[] args) {
+
+   }
+
+   private static void ma() {
+      try {
+
+      } finally {
+
+      }
+   }
+}
+
+
+
---
+++ src/oops/OOPS2.java
@@ -0,0 +1,6 @@
+package oops;
+
+public class OOPS2 {
+   private OOPS2() {
+   }
+}
