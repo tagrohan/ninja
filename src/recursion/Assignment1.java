b0ac8be63f1bbe6700abda9fb7199f715a3002d6
tagrohan <tagrohan@gmail.com>
2021-06-13 22:35:48 +0530

    string to int added

--- src/recursion/Assignment1.java
+++ src/recursion/Assignment1.java
@@ -2,12 +2,32 @@ package recursion;
 
 public class Assignment1 {
    public static void main(String[] args) {
-      System.out.println(replacePi("pixpixp"));
+      System.out.println(convert("00012302"));
+   }
+
+   private static int convert(String arr) {
+//      System.out.println(convert("00012302"));
+//      for (int i = 0; i < arr.length(); i++) {
+//         if (arr.charAt(i) != '0') {
+//            return convertStringToInt(arr.substring(i));
+//         }
+//      }
+      return convertStringToInt(arr);
+   }
+
+   private static int convertStringToInt(String arr) {
+      if (arr.length() == 1) {
+         return Integer.parseInt(arr);
+      }
+      char ch = arr.charAt(0);
+
+      return (int) ((ch - '0') * Math.pow(10, arr.length() - 1) + convertStringToInt(arr.substring(1)));
    }
 
    private static String replacePi(String str) {
-
+//      System.out.println(replacePi("pixpixp"));
       if (str.length() == 0) {
+
          return "";
       }
 
