60cc5baff4d255380e12e4b97115736fb2ac787e
tagrohan <tagrohan@gmail.com>
2021-08-16 02:20:38 +0530

    HIN asked ques added findPair toi k

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Practice {
    public static void main(String[] args) {
 
-      // using for random questions from cNinja
+      System.out.println(findPairTo(new int[]{6,2, 3, 2, 3}, 4));
+   }
+
+   private static boolean findPairTo(int[] arr, int k) {
+      Set<Integer> set = new HashSet<>();
+      for (int i = 0; i < arr.length; i++) {
+         if (set.contains(k - arr[i])) {
+            return true;
+         } else {
+            set.add(arr[i]);
+         }
+      }
+      return false;
    }
 }
